21a5293f8cd634c0bc4c5e1e33c359a746eeb54b
Lowe Kumar <lowekr00@gmail.com>
2024-04-05 19:41:41 +0530

    decorator lecture practice - commit

--- decorators.py
+++ decorators.py
@@ -1,2 +1,63 @@
-def demo():
-    print("decorator tutorial")
+import functools
+# A decorator function takes another function as argument, wraps its behaviour inside
+# an inner function, and returns the wrapped function.
+def start_end_decorator(func):
+    
+    def wrapper():
+        print('Start')
+        func()
+        print('End')
+    return wrapper
+
+def print_name():
+    print('Alex')
+    
+print_name()
+
+print()
+
+# Now wrap the function by passing it as argument to the decorator function
+# and asign it to itself -> Our function has extended behaviour!
+print_name = start_end_decorator(print_name)
+print_name()
+
+print('**************************************************************************')
+def start_end_decorator_2(func):
+    
+    def wrapper(*args, **kwargs):
+        print('Start')
+        result = func(*args, **kwargs)
+        print('End')
+        return result
+    return wrapper
+
+@start_end_decorator_2
+def add_5(x):
+    return x + 5
+
+result = add_5(10)
+print(result)
+
+
+print('*'*75)
+
+# printing greetings x no of times using decorators
+
+def repeat(num_times):
+    def decorator_repeat(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            for _ in range(num_times):
+                result = func(*args, **kwargs)
+            return result
+        return wrapper
+    return decorator_repeat
+
+
+
+@repeat(num_times=3)
+def greet(name):
+    print(f'Hello {name}')
+
+res = greet('XYZ')
+
